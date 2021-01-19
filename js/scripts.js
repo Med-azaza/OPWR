ec8d19ab99f537e268a8fcbdaa5323d83495419e
med azaza <hamaaz666@gmail.com>
2021-01-19 19:58:42 +0100

    champ page edit

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+function myFunction1() {
+    var x = document.getElementById("ab1disc");
+    var a = document.getElementById("ab2disc");
+    var b = document.getElementById("ab3disc");
+    var c = document.getElementById("ab4disc");
+    var d = document.getElementById("ab5disc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("ab2disc");
+    var a = document.getElementById("ab1disc");
+    var b = document.getElementById("ab3disc");
+    var c = document.getElementById("ab4disc");
+    var d = document.getElementById("ab5disc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction3() {
+    var x = document.getElementById("ab3disc");
+    var a = document.getElementById("ab2disc");
+    var b = document.getElementById("ab1disc");
+    var c = document.getElementById("ab4disc");
+    var d = document.getElementById("ab5disc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("ab4disc");
+    var a = document.getElementById("ab2disc");
+    var b = document.getElementById("ab3disc");
+    var c = document.getElementById("ab1disc");
+    var d = document.getElementById("ab5disc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction5() {
+    var x = document.getElementById("ab5disc");
+    var a = document.getElementById("ab2disc");
+    var b = document.getElementById("ab3disc");
+    var c = document.getElementById("ab4disc");
+    var d = document.getElementById("ab1disc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      a.style.display = "none";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+  
