16fe11a6a5f968b419fc95a407cd44c0a5e9e66c
med azaza <hamaaz666@gmail.com>
2021-01-20 00:10:51 +0100

    final resposive

--- js/scripts.js
+++ js/scripts.js
@@ -82,4 +82,9 @@ function myFunction1() {
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
   
