5b86c98f606edfaf859e2d8c40d72f798b83e8d6
Med-azaza <hamaaz666@gmail.com>
2021-03-24 18:53:32 +0100

    add loading screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,7 @@
-/*function myFunction1() {
-    var x = document.getElementById("ab1disc");
-    var a = document.getElementById("ab2disc");
-    var b = document.getElementById("ab3disc");
-    var c = document.getElementById("ab4disc");
-    var d = document.getElementById("ab5disc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "none";
-      d.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function myFunction2() {
-    var x = document.getElementById("ab2disc");
-    var a = document.getElementById("ab1disc");
-    var b = document.getElementById("ab3disc");
-    var c = document.getElementById("ab4disc");
-    var d = document.getElementById("ab5disc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "none";
-      d.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function myFunction3() {
-    var x = document.getElementById("ab3disc");
-    var a = document.getElementById("ab2disc");
-    var b = document.getElementById("ab1disc");
-    var c = document.getElementById("ab4disc");
-    var d = document.getElementById("ab5disc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "none";
-      d.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function myFunction4() {
-    var x = document.getElementById("ab4disc");
-    var a = document.getElementById("ab2disc");
-    var b = document.getElementById("ab3disc");
-    var c = document.getElementById("ab1disc");
-    var d = document.getElementById("ab5disc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "none";
-      d.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  function myFunction5() {
-    var x = document.getElementById("ab5disc");
-    var a = document.getElementById("ab2disc");
-    var b = document.getElementById("ab3disc");
-    var c = document.getElementById("ab4disc");
-    var d = document.getElementById("ab1disc");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "none";
-      d.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-  }
-  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})*/
+$(function() {
+  $('#loading').hide();
   const names=document.getElementsByClassName('name');
   const cards=new Array();
   var x=0;
@@ -111,6 +20,8 @@ box.addEventListener('keyup', function() {
       names[i].parentElement.style.display='none';
     }
   }
+
+})
 
 })
   
